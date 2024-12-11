2581a9845a7e5fcf312598896168201280ce5cb0
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-12-11 14:28:20 +0000

    project settings added ready for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d-%%(8^!xa!$*(bl%s#qq9b=wpr*s05l@^l9i-o%nv^l4s5_te'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = ['8000-cgalpin92-djangoproject-84bhz0bnazn.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-cgalpin92-djangoproject-84bhz0bnazn.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
